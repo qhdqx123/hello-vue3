8c2edc6dcbfec92480043e7891fd9b5da8fcdc1b
qihongdi <hongdi.qi@upuphone.com>
2022-01-13 10:07:37 +0800

    router配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,26 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
-
+  {
+    path: '/',
+    name: 'home',
+    component: () => import(/* webpackChunkName: 'home' */ '@/views/home/index.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: 'login' */ '@/views/login/index.vue')
+  },
+  {
+    path: '/course',
+    name: 'course',
+    component: () => import(/* webpackChunkName: 'course' */ '@/views/course/index.vue')
+  },
+  {
+    path: '/:pathMatch(.*)',
+    name: '404',
+    component: () => import(/* webpackChunkName: '404' */ '@/views/error/index.vue')
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/course/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>课程管理</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
